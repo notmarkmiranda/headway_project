08de738bab05c0f01aee7f2db03a1536614f994a
Mark Miranda <notmarkmiranda@gmail.com>
2017-06-01 19:45:20 -0600

    write feature test for admin creating user

---
+++ spec/features/admin_create_user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'Admin creates user from browser' do
+  scenario 'without admin privileges' do
+    user = create(:user)
+
+    sign_in(user.email, user.password)
+    visit new_admin_user_path
+
+    expect(page).to have_content('You must be an admin to perform that action')
+  end
+
+  scenario 'with admin privileges' do
+    user = create(:user, :admin)
+
+    sign_in(user.email, user.password)
+    visit new_admin_user_path
+
+    within '.form-inputs' do
+      fill_in 'First Name', with: "mark"
+      fill_in 'Last Name', with: "miranda"
+
+      fill_in 'E-Mail Address', with: "mark@miranda.com"
+      fill_in 'Password', with: "password"
+      fill_in 'Password Confirmation', with: "password"
+    end
+
+    click_button 'Create User'
+
+    expect(page).to have_content("mark@miranda.com")
+  end
+end
